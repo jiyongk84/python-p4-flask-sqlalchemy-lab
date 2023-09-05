dbb4d5eab881c65d106b5e4ee74261e71e4ac992
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-09-05 17:16:07 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
-
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
@@ -19,16 +18,71 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
 
+    if animal:
+        response_content = f'<ul>ID: {animal.id}</ul>'
+
+        response_content += f'<ul>Name: {animal.name}</ul>'
+
+        response_content += f'<ul>Species: {animal.species}</ul>'
+
+        zookeeper = animal.zookeeper
+        enclosure = animal.enclosure
+
+        if zookeeper:
+            response_content += f'<ul>Zookeeper: {zookeeper.name}</ul>'
+
+        if enclosure:
+            response_content += f'<ul>Enclosure: {enclosure.environment}</ul>'
+
+        response = make_response(response_content)
+        response.status_code = 200
+        return response
+    else:
+        return '<p>Animal not found</p>', 404
+    
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
-
+    zookeeper = Zookeeper.query.get(id)
+    
+    if zookeeper:
+        response_content = f'<ul>ID: {zookeeper.id}</ul>'
+        response_content += f'<ul>Name: {zookeeper.name}</ul>'
+        response_content += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+        
+        animals = zookeeper.animals
+        
+        if animals:
+            for animal in animals:
+                response_content += f'<ul>Animal: {animal.name}</ul>'
+        
+        response = make_response(response_content)
+        response.status_code = 200
+        return response
+    else:
+        return '<p>Zookeeper not found</p>', 404
+    
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
-
+    enclosure = Enclosure.query.get(id)
+    
+    if enclosure:
+        response_content = f'<ul>ID: {enclosure.id}</ul>'
+        response_content += f'<ul>Environment: {enclosure.environment}</ul>'
+        response_content += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+        
+        animals = enclosure.animals
+        
+    if animals:
+        for animal in animals:
+            response_content += f'<ul>Animal: {animal.name}</ul>'
+        
+        response = make_response(response_content)
+        response.status_code = 200
+        return response
+    else:
+        return '<p>Enclosure not found</p>', 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
